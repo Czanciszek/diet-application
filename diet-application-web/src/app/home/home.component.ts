8e1d1d98da892ce97907a81a27471c70d1b2ab14
Franciszek Czana <franciszek.czana@futurum.tech>
2020-07-18 13:37:20 +0200

    add example get data from backend

--- diet-application-web/src/app/home/home.component.ts
+++ diet-application-web/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RestapiService} from "../restapi.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: any;
+  constructor(
+    private service:RestapiService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  getUsers() {
+    let response = this.service.getUsers();
+    response.subscribe(data => {
+      this.users = data;
+    });
+  }
 }
