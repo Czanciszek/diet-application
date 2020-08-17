3c047e3d05e7c568a90a1194d0e18a4e4914e1a1
Franciszek Czana <franciszek.czana@futurum.tech>
2020-08-18 00:55:52 +0200

    Change products table list to angular materials

---
+++ diet-application-web/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../service/product.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {FormControl} from "@angular/forms";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {ProductComponent} from "../product/product.component";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor(
+    private service: ProductService,
+    private dialog: MatDialog,
+  ) { }
+
+  listData: MatTableDataSource<any>;
+  displayedColumns: string[] = ['category', 'subcategory', 'name',
+                                'energyValue', 'proteins', 'fats', 'carbohydrates',
+                                'alergens', 'actions'];
+
+  ngOnInit(): void {
+    this.service.getProducts().subscribe(
+      list => {
+        let array = list.map(item => {
+          return {
+            id: item.id,
+            header: item.header,
+            primaryImageId: item.primaryImageId,
+            type: item.type,
+            name: item.name,
+            category: item.category,
+            subcategory: item.subcategory,
+            energyValue: item.energyValue,
+            proteins: item.proteins,
+            fats: item.fats,
+            saturatedFattyAcids: item.saturatedFattyAcids,
+            monoUnsaturatedFattyAcids: item.monoUnsaturatedFattyAcids,
+            polyUnsaturatedFattyAcids: item.polyUnsaturatedFattyAcids,
+            cholesterol: item.cholesterol,
+            carbohydrates: item.carbohydrates,
+            sucrose: item.sucrose,
+            dietaryFibres: item.dietaryFibres,
+            sodium: item.sodium,
+            potassium: item.potassium,
+            calcium: item.calcium,
+            phosphorus: item.phosphorus,
+            magnesium: item.magnesium,
+            iron: item.iron,
+            selenium: item.selenium,
+            betaCarotene: item.betaCarotene,
+            vitaminD: item.vitaminD,
+            vitaminC: item.vitaminC,
+            lactose: item.lactose,
+            starch: item.starch,
+            gluten: item.gluten,
+          };
+        });
+        this.listData = new MatTableDataSource(array);
+      });
+  }
+
+  onEdit(product) {
+    this.service.populateForm(product);
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    dialogConfig.width = "90%";
+
+    this.dialog.open(ProductComponent, dialogConfig);
+  }
+
+  onDelete(productId) {
+    if (confirm("Are you sure to delete this product?"))
+      this.service.deleteProduct(productId);
+  }
+}
