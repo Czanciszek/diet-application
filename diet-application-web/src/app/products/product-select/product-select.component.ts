f95cfcc81500d823606717f523630cc09877cea8
Franciszek Czana <franciszek.czana@futurum.tech>
2021-03-03 19:12:43 +0100

    Fix not displaying food properties on product select

--- diet-application-web/src/app/products/product-select/product-select.component.ts
+++ diet-application-web/src/app/products/product-select/product-select.component.ts
@@ -1,9 +1,10 @@
-import {Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProductService} from "../../service/product.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatDialogRef} from "@angular/material/dialog";
 import {MatSort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
+import {Product} from "../../model/product";
 
 @Component({
   selector: 'app-product-select',
@@ -35,49 +36,6 @@ export class ProductSelectComponent implements OnInit {
     this.getCategories();
   }
 
-  getProducts() {
-    this.service.getProducts().subscribe(
-      list => {
-        let array = list.map(item => {
-          return {
-            id: item.id,
-            header: item.header,
-            primaryImageId: item.primaryImageId,
-            type: item.type,
-            name: item.name,
-            category: item.category,
-            subcategory: item.subcategory,
-            energyValue: item.energyValue,
-            proteins: item.proteins,
-            fats: item.fats,
-            saturatedFattyAcids: item.saturatedFattyAcids,
-            monoUnsaturatedFattyAcids: item.monoUnsaturatedFattyAcids,
-            polyUnsaturatedFattyAcids: item.polyUnsaturatedFattyAcids,
-            cholesterol: item.cholesterol,
-            carbohydrates: item.carbohydrates,
-            sucrose: item.sucrose,
-            dietaryFibres: item.dietaryFibres,
-            sodium: item.sodium,
-            potassium: item.potassium,
-            calcium: item.calcium,
-            phosphorus: item.phosphorus,
-            magnesium: item.magnesium,
-            iron: item.iron,
-            selenium: item.selenium,
-            betaCarotene: item.betaCarotene,
-            vitaminD: item.vitaminD,
-            vitaminC: item.vitaminC,
-            lactose: item.lactose,
-            starch: item.starch,
-            gluten: item.gluten,
-          };
-        });
-        this.listData = new MatTableDataSource(array);
-        this.listData.sort = this.sort;
-        this.listData.paginator = this.paginator;
-      })
-  }
-
   getCategories() {
     let response = this.service.getCategories();
     response.subscribe(data => {
@@ -94,45 +52,12 @@ export class ProductSelectComponent implements OnInit {
   onSearchButtonClick() {
     this.service.getFilteredProducts(this.selectedCategory, this.selectedSubcategory)
       .subscribe(
-        list => {
-          let array = list.map(item => {
-            return {
-              id: item.id,
-              header: item.header,
-              primaryImageId: item.primaryImageId,
-              type: item.type,
-              name: item.name,
-              category: item.category,
-              subcategory: item.subcategory,
-              energyValue: item.energyValue,
-              proteins: item.proteins,
-              fats: item.fats,
-              saturatedFattyAcids: item.saturatedFattyAcids,
-              monoUnsaturatedFattyAcids: item.monoUnsaturatedFattyAcids,
-              polyUnsaturatedFattyAcids: item.polyUnsaturatedFattyAcids,
-              cholesterol: item.cholesterol,
-              carbohydrates: item.carbohydrates,
-              sucrose: item.sucrose,
-              dietaryFibres: item.dietaryFibres,
-              sodium: item.sodium,
-              potassium: item.potassium,
-              calcium: item.calcium,
-              phosphorus: item.phosphorus,
-              magnesium: item.magnesium,
-              iron: item.iron,
-              selenium: item.selenium,
-              betaCarotene: item.betaCarotene,
-              vitaminD: item.vitaminD,
-              vitaminC: item.vitaminC,
-              lactose: item.lactose,
-              starch: item.starch,
-              gluten: item.gluten,
-            };
-          });
-          this.listData = new MatTableDataSource(array);
+        (data: Product[]) => {
+          const productList = [ ...data];
+          this.listData = new MatTableDataSource(productList);
           this.listData.sort = this.sort;
           this.listData.paginator = this.paginator;
-        })
+        });
   }
 
   onSelect(product) {
